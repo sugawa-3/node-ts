ff12f8e4a625e6870f99fd5ce7a17437b77ec024
sugawa <eri.sugawa@dragon.jp>
2020-09-23 19:52:27 +0900

    add first environment

---
+++ __tests__/index.test.ts
@@ -0,0 +1,8 @@
+// import {hello} from "../index";
+const index = require('../src/index');
+
+describe("@/indexのテスト", () => {
+    it("テスト", () => {
+        expect(index.hello("World")).toBe("Hello, World!");
+    });
+});
