69109886b886b02c7f08ed2b7e811d269d77a588
sugawa <eri.sugawa@dragon.jp>
2020-09-24 11:46:04 +0900

    change index.test.ts

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -4,5 +4,6 @@ const index = require('../src/index');
 describe("@/indexのテスト", () => {
     it("テスト", () => {
         expect(index.hello("World")).toBe("Hello, World!");
+        expect(index.hello("World")).toBe("Hello");
     });
 });
